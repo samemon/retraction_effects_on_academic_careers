5e34ff10157f17e78b19c0ce1b89a1e288e475a0
Shahan Ali Memon <shahan.ali.memon@gmail.com>
2023-12-07 01:47:05 -0800

    Added first draft of code to extract matches for papers from OA

--- code/preprocessing/job_script.sh
+++ code/preprocessing/job_script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash                                                                                                                                         
 
-#SBATCH -p bigmem #comment it out if you don't want to use the condo by adding extra #
-##SBATCH -p compute
+##SBATCH -p bigmem #comment it out if you don't want to use the condo by adding extra #
+#SBATCH -p compute
 #SBATCH -n 1
 ##SBATCH -q css
 #SBATCH -a 1-60
-#SBATCH --mem=1800GB #499GB is max when using condo #119GB doesn't require bigmem
-#SBATCH -t  12:00:00 #times out after 48 hours
-#SBATCH -o job.%J.openalex.out  #output file
-#SBATCH -e job.%J.openalex.err  #input file                                                                                                                                
+#SBATCH --mem=480GB #499GB is max when using condo #119GB doesn't require bigmem
+#SBATCH -t  24:00:00 #times out after 48 hours
+#SBATCH -o job.%J.fuzz.out  #output file
+#SBATCH -e job.%J.fuzz.err  #input file                                                                                                                                
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=sm9654@nyu.edu
 
