ec8c011436216afabebf91968d5c15a17cbd7426
Shahan Ali Memon <shahan.ali.memon@gmail.com>
2023-12-27 22:36:30 -0800

    added first draft of exact matching:

--- code/preprocessing/0a.extract_mag_paper_matches_exact.py
+++ code/preprocessing/0a.extract_mag_paper_matches_exact.py
@@ -10,80 +10,32 @@ This is the first of three parts
 of fuzzy matching where we will match 
 based on 
 1) doi
-2) pmid
-3) exact title match
+2) exact title match
 
 In later parts we will match based on 
-4) fuzzy title match in the same year
-5) fuzzy title match in the year +- 1
+3) fuzzy title match in the same year
+4) fuzzy title match in the year +- 1
 """
 
 import pandas as pd
 import os
-import argparse
 from config_reader import read_config
 from data_utils import read_csv
-from rapidfuzz import process, fuzz
 
-def find_paper_matches(retracted_papers, path_oa_works, year, nchoices=3):
-    """This function will find string matches for retracted paper 
-    titles from open alex works using fuzzy matching. For now, we will 
-    find 3 matches per paper. For each paper we will only look at 
-    papers from the year +-1 of the retracted paper's publication year.
-    
-    Args:
-        retracted_papers ([list]): list of titles of retracted papers
-        path_oa_works ([string]): path to Open Alex works object
-        year ([int/float]): year of the retracted papers
-        nchoices ([int]): number of matches per paper
-    """
-    
-    
-    df_works = pd.read_csv(path_oa_works, 
-                        usecols=['id','title','publication_year'])
-    
-    # extracting relevant works
-    df_works['YearDiff'] = df_works['publication_year'] - year
-    df_works = df_works[df_works['YearDiff'].isin([-1,0,1])]\
-                    .drop(columns=['YearDiff'])\
-                    .rename(columns={'id':'work_id'})
-    # reseting index so we can track the id's later
-    df_works = df_works.reset_index(drop=True)
-    # normalizing the title
-    df_works['NormTitle'] = df_works['title'].str.lower().str.strip()
-    
-    # normalizing titles of retracted papers
-    retracted_papers = [title.lower().strip() for title in retracted_papers]
-    
-    # We will save matches in this list
-    lst_matches = []
-    
-    # Finding the top matches for each paper 
-    for title in retracted_papers:
-        matches = process.extract(title, df_works['NormTitle'], limit=nchoices)
-        # processing matches
-        for match,score,index in matches:
-            matched_id_title_pubyear = df_works.loc[index].tolist()
-            lst_matches.append([title, match, score, index]+matched_id_title_pubyear)
-    
-    columns=['RWTitle','OAMatchTitle','score', 'index', 'work_id',
-                'OATitle','publication_year','OANormTitle']
-    
-    return pd.DataFrame(lst_matches, columns=columns)
-
-def main(year):
+def main():
     # reading all the relevant paths
     paths = read_config()
     OUTDIR_FUZZYMATCH_PATH = paths['OUTDIR_FUZZYMATCH_PATH']
     # Add path to your RW Original dataset
     RW_CSV_PATH = paths['RW_CSV_PATH']
-    # Add path to your OpenAlex works.csv.gz file
-    OA_WORKS_PATH = paths['OA_WORKS_PATH']
+    # Add path to your MAG papers file
+    MAG_PAPERS_PATH = paths['MAG_PAPERS_PATH']
     
     # Read datasets
-    df_rw = read_csv(RW_CSV_PATH, ['Title', 'OriginalPaperDate'])\
-                    .drop_duplicates()
+    # Reading retraction watch with the relevant columns only
+    df_rw = pd.read_csv(RW_CSV_PATH).drop_duplicates()
     
+    # preprocessing original paper date for later (part b of paper matching)
     # extracting publication year
     # some of these will have no publication date as a result of this
     df_rw['OriginalPaperDate'] = pd.to_datetime(df_rw['OriginalPaperDate'], 
@@ -91,31 +43,61 @@ def main(year):
                                                 errors='coerce')
     
     df_rw['OriginalPaperYear'] = df_rw['OriginalPaperDate'].dt.year
+    
+    # processing title
+    df_rw['RWTitleNorm'] = df_rw['Title'].str.lower() 
+    
+    # preprocessing retraction date to remove all that were retracted post 2015
+    df_rw['RetractionDate'] = pd.to_datetime(df_rw['RetractionDate'], 
+                                                format='%d/%m/%Y %H:%M', 
+                                                errors='coerce')
+    
+    df_rw['RetractionYear'] = df_rw['RetractionDate'].dt.year
+    
+    # Let us identify all papers retracted between 1990 and 2015 (inclusive)
+    # Let us also print # of papers that fall out of this range. 
+    
+    num_papers_1990_2015 = df_rw[df_rw['RetractionYear'].ge(1990) & df_rw['RetractionYear'].le(2015)]\
+                                ['Record ID'].nunique()
+    
+    num_papers_not_1990_2015 = df_rw[df_rw['RetractionYear'].lt(1990) | df_rw['RetractionYear'].gt(2015)]\
+                                ['Record ID'].nunique()
+    
+    print(f"Number of retracted papers between 1990 and 2015 {num_papers_1990_2015}")
+    print(f"Number of retracted papers beyond 1990 and 2015 {num_papers_not_1990_2015}")
 
-    # filtering
-    df_rw = df_rw[df_rw.OriginalPaperYear.eq(year) | df_rw.OriginalPaperYear.isna()]
+    # filtering (Note only one paper has NaN retraction year from 1700s)
+    df_rw = df_rw[df_rw['RetractionYear'].ge(1990) & df_rw['RetractionYear'].le(2015)]
     
-    retracted_papers = df_rw['Title'].unique().tolist()
+    # reading the MAG papers.txt
+    df_papers = pd.read_csv(MAG_PAPERS_PATH, sep="\t", header=None, 
+                            usecols=[0,2,4])\
+                                .rename(columns={0:'MAGPID',
+                                                2:'OriginalPaperDOI',
+                                                4:'MAGTitle'})
     
-    num_retracted_papers = len(retracted_papers)
-    print(f"Number of retracted papers processing in {year}: {num_retracted_papers}")
+    # only extracting relevant columns
+    df_rw_relevant = df_rw[['Record ID', 'RWTitleNorm', 'OriginalPaperDOI']].drop_duplicates()
     
-    df_matched = find_paper_matches(retracted_papers, OA_WORKS_PATH, year)
+    # merging on doi
+    df_merged_doi = df_rw_relevant.merge(df_papers, on='OriginalPaperDOI', 
+                                            how='inner')
+    
+    # merging on title
+    df_merged_title = df_rw_relevant.merge(df_papers, left_on='RWTitleNorm', right_on='MAGTitle', 
+                                            how='inner')
+    
+    # concatenating
+    df_matched = pd.concat([df_merged_doi, df_merged_title])
     
     # Save the relevant data
-    filename = f"works_ids_RW_OA_fuzzymatched_{year}.csv"
-    df_matched.to_csv(os.path.join(OUTDIR_FUZZYMATCH_PATH, filename), index=False)
+    
+    # We shall save this version of retraction watch to use it for later processing.
+    df_rw.to_csv(os.path.join(OUTDIR_FUZZYMATCH_PATH, "RW_1990_2015.csv", index=False))
+    
+    df_matched.to_csv(os.path.join(OUTDIR_FUZZYMATCH_PATH, "RW_MAG_exact_paper_matched.csv"), 
+                        index=False)
     
     
 if __name__ == "__main__":
-    # Set up argument parser
-    parser = argparse.ArgumentParser(description="Fuzzy-matching paper titles")
-    
-    # Add command line argument for the year
-    parser.add_argument("--year", type=float, help="Year of publication for retracted papers", required=True)
-
-    # Parse the command line arguments
-    args = parser.parse_args()
-
-    # Call the main function with the parsed year argument
-    main(args.year)
+    main()
