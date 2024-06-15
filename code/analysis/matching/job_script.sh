73a60addf7acabc9de39d3bd4c0ac3f09bac197d
Shahan Ali Memon <shahan.ali.memon@gmail.com>
2024-06-15 15:06:53 -0700

    Revert "Commiting all matching code for now"
    This reverts commit e160d2cb7ce28ec920c09b95496b6b42cacce6a2.

--- code/analysis/matching/job_script.sh
+++ code/analysis/matching/job_script.sh
@@ -4,15 +4,15 @@
 #SBATCH -p compute
 #SBATCH -n 1
 ##SBATCH -q css
-#SBATCH -a 1-100
+##SBATCH -a 1-26
 #SBATCH --mem=400GB #499GB is max when using condo #119GB doesn't require bigmem
-#SBATCH -t  48:00:00 #times out after 48 hours
-#SBATCH -o job.%J.tc.out  #output file
-#SBATCH -e job.%J.tc.err  #input file                                                                                                                                
+#SBATCH -t  2:00:00 #times out after 48 hours
+#SBATCH -o job.%J.collabs30.out  #output file
+#SBATCH -e job.%J.collabs30.err  #input file                                                                                                                                
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=sm9654@nyu.edu
 
-srun $(head -n $SLURM_ARRAY_TASK_ID jobs.txt | tail -n 1)
+#srun $(head -n $SLURM_ARRAY_TASK_ID jobs.txt | tail -n 1)
 
 #python3 1.gender_firstyear_firstaff.py
 #python3 merge_firstaff_files.py
@@ -26,5 +26,4 @@ srun $(head -n $SLURM_ARRAY_TASK_ID jobs.txt | tail -n 1)
 #python3 9.extracting_activity_AllMatchesUntilCategorical.py
 #python3 10.filter_authors_and_multiple_aff.py
 #python3 11.closest_match_papers_citations_collaborators.py
-# python3 12.process_1d_collabs.py
-#python3 16.extracting_2D_collaborators_closestmatches_rematching.py
+python3 12.process_1d_collabs.py
